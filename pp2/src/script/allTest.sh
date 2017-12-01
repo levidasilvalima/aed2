e755cdade376ad247b4a365265685aa0e3d89449
Rodrigo Moraes <rdcmdev@gmail.com>
2017-12-01 16:28:01 -0400

    chore: update to include input file by shell script

--- pp2/src/script/allTest.sh
+++ pp2/src/script/allTest.sh
@@ -2,7 +2,10 @@ SEARCH_DIRECTORY=$1
 
 for entry in $SEARCH_DIRECTORY*
 do
-  ./script/compile-execute.sh $entry run
+    if ! grep -q "GraphBrain" $entry
+        then
+            ./script/compile-execute.sh $entry run
+    else
+        ./script/compile-execute.sh $entry run < cases/1.in
+    fi
 done
-
-
