17476a4e11cf7f4d985765a9d22b3e56a49a2dbf
Levi da Silva Lima <levidasilvalima.ldsl@gmail.com>
2017-12-03 02:54:09 -0500

    feat: add Adapter
    1. Graph to Edge List
    2. Edge List to Graph

---
+++ pp2/src/lib/Adapter.h
@@ -0,0 +1,46 @@
+# include "Graph.h"
+# include "DoubleLinkedList.h"
+# include "Edge.h"
+# include "Neuron.h"
+
+# ifndef GRAPH_ADAPTER_H
+# define GRAPH_ADAPTER_H
+
+class Adapter {
+public:
+    Adapter();
+	void getEdgeList(Graph graph, DoubleLinkedList<Edge>& graphEdgeList);
+	void getGraph(DoubleLinkedList<Neuron> graphNeuronList, Graph& graph);
+};
+
+Adapter::Adapter() {
+
+}
+
+void Adapter::getEdgeList(Graph graph, DoubleLinkedList<Edge>& graphEdgeList){
+	int graphOrder = graph.getOrder();
+	for(int u=1; u < graphOrder - 1; u++){
+		for(int v=u+1; v < graphOrder; v++){
+			if(graph.getWeight(u, v) != 0){
+				Edge newEdge = Edge(u, v, graph.getWeight(u, v));
+				graphEdgeList.insert(newEdge);
+			}
+		}
+	}
+}
+
+void Adapter::getGraph(DoubleLinkedList<Neuron> graphNeuronList, Graph& graph){
+	int size = graphNeuronList.size();
+	Neuron neuron;
+	int source, destination;
+	double weight;
+	for(int i = 0; i < size; i++){
+		neuron = graphNeuronList.get(i);
+		source = neuron.getSource();
+		destination = neuron.getDestination();
+		weight = neuron.getWeight();
+		graph.insertEdge(source, destination, weight);
+	}
+}
+
+# endif
